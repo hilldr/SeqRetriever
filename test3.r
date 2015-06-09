813508bfaa93aeb940523ca5124fe130c299bff3
Shrikar Thodla <shrikar_t@yahoo.com>
2015-06-09 15:00:06 -0400

    Message output_format 1

---
+++ test3.r
@@ -0,0 +1,6 @@
+## Test case 3
+# Load function "gene_retriever_png"
+#Tests to see if new gene retriever png works
+source("gene_retriever_png.r")
+gene_retriever(gene_names=c("OR4F5","SAMD11","AJAP1","SKI","ESPN", "CNKSR1"),nrow=2,dir="./DATA/norm_out", csv.out= "output3.csv", png= "gr_output4.png")
+

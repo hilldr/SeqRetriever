c279b6be1cedefe5c81f6df5421f1c39ea3e496f
hilldr <hilldr@med.umich.edu>
2015-07-10 09:53:35 -0400

    Setup GeneRetriever R package
          - Used the following guide to establish basic requirements
          http://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/

--- test3.r
+++
@@ -1,6 +0,0 @@
-## Test case 3
-# Load function "gene_retriever_png"
-#Tests to see if new gene retriever png works
-source("gene_retriever.r")
-gene_retriever(gene_names=c("OR4F5","SAMD11","AJAP1","SKI","ESPN", "CNKSR1"),nrow=2,dir="./DATA/norm_out", csv.out= "output3.csv", gr_name= "gr_output4.png", pdf=FALSE)
-
--- test_wrong.r
+++
@@ -1,5 +0,0 @@
-## Test case 4
-# Load function "Gene Retriever"
-#Tests to see if gene retriever function provides an error message that specifies which genes were not found
-source("gene_retriever.r")
-gene_retriever(gene_names=c("O5","SAMD11","AJAP1","SKI","ESPN", "CNKSR1"),nrow=2,dir="./DATA/norm_out", csv.out= "output_wrong.csv", gr_name= "gr_output_wrong.pdf")

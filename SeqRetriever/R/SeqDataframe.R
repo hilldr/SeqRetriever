aa4e596657cd8adf808352335b4d7c08ef0b8877
hilldr <hilldr@med.umich.edu>
2016-03-02 15:46:35 -0500

    Replace plyr with dplyr
    	- 1,154% performance increase for SeqDataframe()

--- SeqRetriever/R/SeqDataframe.R
+++ SeqRetriever/R/SeqDataframe.R
@@ -13,24 +13,29 @@ SeqDataframe <- function(dir = "./"){
   ######################################
   ## IMPORT DATA FROM CUFFNORM OUTPUT ##
   ######################################
-  # dir.count is a string for the count table location
-  dir.count <- paste(dir, "/genes.count_table", sep="")
-  # read in the count table from dir.count
-  counts <- read.table(dir.count, header=TRUE, sep="\t", stringsAsFactors = FALSE)
-  counts$tracking_id <- NULL
-  # Read in data attributes from genes.attr_table file
-  dir.attr <- paste(dir,"/genes.attr_table", sep="")
-  cn.attr <- read.table(dir.attr, header=TRUE, sep="\t", stringsAsFactors = FALSE)
-  # Bind the gene_short_name from the attr.table to data1,
-  gene_short_name <- as.character(cn.attr[,"gene_short_name"])
-  data1 <- cbind(gene_short_name, counts)
-  data1$gene_short_name <- as.character(data1$gene_short_name)
-  # load library plyr
-  library(plyr)
-  # Sum counts for gene isoforms  
-  data1 <- ddply(data1, "gene_short_name", numcolwise(sum))
-  rownames(data1) <- data1$gene_short_name  
-  return(data1)
+    # dir.count is a string for the count table location
+    dir.count <- paste(dir, "/genes.count_table", sep="")
+    # read in the count table from dir.count
+    counts <- read.table(dir.count, header=TRUE, sep="\t", stringsAsFactors = FALSE)
+    counts$tracking_id <- NULL
+    # Read in data attributes from genes.attr_table file
+    dir.attr <- paste(dir,"/genes.attr_table", sep="")
+    cn.attr <- read.table(dir.attr, header=TRUE, sep="\t", stringsAsFactors = FALSE)
+    # Bind the gene_short_name from the attr.table to data1,
+    gene_short_name <- as.character(cn.attr[,"gene_short_name"])
+    data1 <- cbind(gene_short_name, counts)
+    data1$gene_short_name <- as.character(data1$gene_short_name)
+    # load library plyr
+    library(dplyr)
+    
+    # Sum counts for gene isoforms
+    data1 <- data1 %>%
+        group_by(gene_short_name) %>%
+        summarise_each(funs(sum)) %>%
+        as.data.frame()
+    
+    rownames(data1) <- data1$gene_short_name  
+    return(data1)
 }
 
 ## SeqRetriever

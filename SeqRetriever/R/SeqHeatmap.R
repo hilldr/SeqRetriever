5ec0905fb784379a3be847abf00694f6bc6d1d08
David R. Hill <hilldr@med.umich.edu>
2016-03-10 21:25:31 -0500

    Fix extraneous code in SeqHeatmap()

--- SeqRetriever/R/SeqHeatmap.R
+++ SeqRetriever/R/SeqHeatmap.R
@@ -26,10 +26,6 @@ SeqHeatmap <- function(df,
                        dist.method = "euclidean",
                        hclust.method = "ward.D")
 {
-
-    library(SeqRetriever)
-    testdf <- SeqDataframe(dir = "./norm_out") # format dataframe
-    genes <- SeqGenes(gene.names = c("OR4F5","SAMD11","AJAP1","SKI","ESPN", "CNKSR1"), df = testdf)
     df <- df[apply(df, 1, sd, na.rm = TRUE) != 0,] # remove genes when Std. Dev. = 0
     rownames(df) <- df$gene_short_name
     df$gene_short_name <- NULL
@@ -50,9 +46,10 @@ SeqHeatmap <- function(df,
     library(ggplot2)
     library(scales) # needed for "muted" colors
     plot <- ggplot(df.plot, aes(x = sample, y = gene, fill = expression)) +
-        geom_tile(color = "black", lwd = 0.5) +
+        geom_tile() +
         scale_fill_gradient2("Z-score",low = muted("blue"), high = muted("red")) +
-        coord_fixed(ratio = 1) + # fix aspect ratio
+       # coord_fixed(ratio = 1) + # fix aspect ratio
+       # geom_tile(color = "black", lwd = 0.5) + # set color and size of grid
         theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
               panel.background = element_blank()) # reasonable defaults
 
